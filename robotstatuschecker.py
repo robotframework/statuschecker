1f007cf4303204fc57bf2249073d23041b4976f8
Tatu Aalto <aalto.tatu@gmail.com>
2021-10-25 20:52:28 +0300

    Updated version to 2.1.0

--- robotstatuschecker.py
+++ robotstatuschecker.py
@@ -45,7 +45,7 @@ from robot import __version__ as rf_version
 from robot.api import ExecutionResult, ResultVisitor
 from robot.utils import Matcher
 
-__version__ = "2.0.4.dev1"
+__version__ = "2.1.0"
 RF3 = rf_version.startswith("3")
 
 
